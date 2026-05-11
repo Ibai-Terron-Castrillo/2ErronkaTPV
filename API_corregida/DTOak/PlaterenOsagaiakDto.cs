c8650c8da8d5677dcd485b190dbc6754ecb0984e
Beñat Aranzadi Duran <aranzadi99@gmail.com>
2026-05-11 15:02:12 +0200

    TPV-a zuzenduta

--- API_corregida/DTOak/PlaterenOsagaiakDto.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace JatetxeaApi.DTOak
-{
-    public class PlaterenOsagaiakDto
-    {
-        public int PlateraId { get; set; }
-        public int InbentarioaId { get; set; }
-        public decimal Kantitatea { get; set; }
-    }
-}
